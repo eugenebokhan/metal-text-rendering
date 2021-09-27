6d984fc8ba3d6503cb0dd5b50c66f8082124409c
eugene <eugenebokhan@icloud.com>
2021-09-27 18:19:12 +0300

    migrate to metal rendering tools

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Metal-Text-Rendering
 
-[Metal](https://developer.apple.com/metal/) text rendering example app that uses [Alloy](https://github.com/s1ddok/Alloy/tree/text-rendering) under the hood.
+[Metal](https://developer.apple.com/metal/) text rendering example app that uses [MetalRenderingTools](https://github.com/eugenebokhan/metal-rendering-tools) under the hood.
--- TextRendering/ViewController.swift
+++ TextRendering/ViewController.swift
@@ -1,64 +1,68 @@
 import UIKit
 import SnapKit
-import Alloy
-import MetalView
+import MetalRenderingTools
+import TextureView
 
 class ViewController: UIViewController {
 
     // MARK: - Properties
 
     // UI
-    private var metalView: MetalView!
+    private var textureView: TextureView!
     // Core
     private var context: MTLContext!
     private var atlasProvider: MTLFontAtlasProvider!
     private var textRender: TextRender!
-    private var destinationTexture: MTLTexture!
 
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        do { try self.setup() }
-        catch { fatalError() }
+        try! self.setup()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
         self.setupTextRender()
-        self.draw(texture: self.destinationTexture)
+        self.draw()
     }
 
     // MARK: - Setup
 
     private func setup() throws {
-        self.context = try .init()
-        self.metalView = try .init(context: self.context)
-        self.metalView.contentScaleFactor = UIScreen.main.scale
-
         let screenBounds = UIScreen.main.nativeBounds
-        self.destinationTexture = try self.context
-                                          .texture(width: .init(screenBounds.width),
-                                                   height: .init(screenBounds.height),
-                                                   pixelFormat: .bgra8Unorm)
+        self.context = try .init()
+        self.textureView = try .init(
+            device: self.context.device,
+            pixelFormat: .bgra8Unorm
+        )
+        self.textureView.contentScaleFactor = UIScreen.main.scale
+        self.textureView.texture = try self.context.texture(
+            width: .init(screenBounds.width),
+            height: .init(screenBounds.height),
+            pixelFormat: .bgra8Unorm
+        )
 
         self.atlasProvider = try MTLFontAtlasProvider(context: self.context)
-        let fontAtlas = try self.atlasProvider
-                                .fontAtlas(descriptor: MTLFontAtlasProvider.defaultAtlasDescriptor)
-        self.textRender = try .init(context: self.context,
-                                    fontAtlas: fontAtlas)
+        let fontAtlas = try self.atlasProvider.fontAtlas(
+            descriptor: MTLFontAtlasProvider.defaultAtlasDescriptor
+        )
+        self.textRender = try .init(
+            context: self.context,
+            fontAtlas: fontAtlas
+        )
         self.setupUI()
     }
 
     private func setupUI() {
-        self.metalView.layer.cornerRadius = 10
-        self.metalView.layer.masksToBounds = true
+        self.textureView.layer.cornerRadius = 10
+        self.textureView.layer.masksToBounds = true
 
-        self.view.addSubview(self.metalView)
+        self.view.addSubview(self.textureView)
 
-        self.metalView.snp.makeConstraints { constraintMaker in
+        self.textureView.snp.makeConstraints { constraintMaker in
             constraintMaker.top.equalToSuperview().inset(40)
             constraintMaker.bottom.equalToSuperview().inset(40)
             constraintMaker.trailing.leading.equalToSuperview()
@@ -66,26 +70,34 @@ class ViewController: UIViewController {
     }
 
     private func setupTextRender() {
-        self.textRender.renderTargetSize = self.destinationTexture.size
+        guard let texture = self.textureView.texture
+        else { return }
+        self.textRender.renderTargetSize = texture.size
         self.textRender.geometryDescriptors = [
             .init(text: "It’s time to kick ass and chew bubble gum...and I’m all outta gum.",
-                  normalizedRect: .init(origin: .init(x: 0.25, y: 0.25),
-                                        size: .init(width: 0.5,
-                                                    height: 0.5)),
+                  normalizedRect: .init(
+                    origin: .init(
+                        x: 0.25,
+                        y: 0.25
+                    ),
+                    size: .init(
+                        width: 0.5,
+                        height: 0.5
+                    )
+                  ),
                   color: UIColor.red.cgColor)
         ]
     }
 
     // MARK: - Draw
 
-    private func draw(texture: MTLTexture) {
-        do {
-            try self.context.schedule { commandBuffer in
-                self.metalView.draw(texture: texture,
-                                    additionalRenderCommands: (self.textRender.render(using:)),
-                                    in: commandBuffer)
-            }
-        } catch { return }
+    private func draw() {
+        try? self.context.schedule { commandBuffer in
+            self.textureView.draw(
+                additionalRenderCommands: self.textRender.render(using:),
+                in: commandBuffer
+            )
+        }
     }
 
 }
